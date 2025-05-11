97a47a30fa4ffe38b404dbd54adc7108dbd10ec9
Yadav Manjay <140719451+CODING-ENJOY154@users.noreply.github.com>
2025-05-11 09:58:31 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -9,7 +9,7 @@ const HeroPic = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0 }}
-      className=" h-full flex items-center justify-center "
+      className="h-full flex items-center justify-center relative"
     >
       <img
         src="/images/hexapic.png"
@@ -17,8 +17,8 @@ const HeroPic = () => {
         className="max-h-[450px] w-auto"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
-        <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
+      <div className="absolute -z-10 flex justify-center items-center animate-pulse">
+        <PiHexagonThin className="md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
       </div>
     </motion.div>
   );
