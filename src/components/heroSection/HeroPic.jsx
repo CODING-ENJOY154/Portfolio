59bb4f029a00c075e9877d6bdbbc5c3292a080b7
Yadav Manjay <140719451+CODING-ENJOY154@users.noreply.github.com>
2025-05-11 09:57:41 +0530

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="images/hexapic.png"
+        src="/images/hexapic.png"
         alt="yadav manjay"
         className="max-h-[450px] w-auto"
       />
